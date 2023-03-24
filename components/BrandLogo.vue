e0fb3867e0adcf408c2634a110ceb55d7665f34d
Rokhman Assidik <38391338+Switch30@users.noreply.github.com>
2023-03-24 13:16:43 +0700

    Initiate

---
+++ components/BrandLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="24" height="24" fill="none"/>
+    <rect x="6.60641" y="1.91061" width="20.0157" height="4.54909" rx="2.27454" transform="rotate(90 6.60641 1.91061)" stroke="currentColor" stroke-width="2"/>
+    <rect x="-0.0210569" y="1.04666" width="26.3673" height="4.57783" rx="2.28891" transform="matrix(0.701996 0.712181 -0.730072 0.683371 5.19512 0.346401)" stroke="currentColor" stroke-width="2"/>
+    <path d="M22.939 4.10797C22.939 5.30338 21.939 6.30533 20.6644 6.30533C19.3898 6.30533 18.3899 5.30338 18.3899 4.10797C18.3899 2.91256 19.3898 1.91061 20.6644 1.91061C21.939 1.91061 22.939 2.91256 22.939 4.10797Z" stroke="currentColor" stroke-width="2"/>
+  </svg>
+</template>
